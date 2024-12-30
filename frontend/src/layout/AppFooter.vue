cfa772d4a35e99f3cee2c913971e198b2d784bef
Harry-zklcdc <zklcdc@qq.com>
2024-12-30 21:48:47 +0800

    [Chore] 📌 Modify Opensource Link

--- frontend/src/layout/AppFooter.vue
+++ frontend/src/layout/AppFooter.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="layout-footer">
     © 2024-Precent 晓声智能科技 |
-    <a href="https://www.xsheng-ai.com" target="_blank" rel="noopener noreferrer"
+    <a href="https://github.com/XShengTech/MEGREZ" target="_blank" rel="noopener noreferrer"
       class="text-primary font-bold hover:underline">MEGREZ 天权算能聚联计算平台</a>
 
   </div>
