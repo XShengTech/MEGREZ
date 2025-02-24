81b52e80b52728d90ddb806d59138dd996c45de7
Harry-zklcdc <zklcdc@qq.com>
2025-02-24 23:38:04 +0800

    [Fix] 🐛 Fix Regexp Pattern to Support subdomain Email #15

--- libs/utils/email.go
+++ libs/utils/email.go
@@ -5,7 +5,7 @@ import (
 )
 
 func EmailFormat(email string) bool {
-	pattern := `^\w+(-+.\w+)*@\w+(-.\w+)*.\w+(-.\w+)*$`
+	pattern := `\w[-\w.+]*@([-A-Za-z0-9]+\.)+[A-Za-z]{2,14}`
 	match, err := regexp.MatchString(pattern, email)
 	if err != nil {
 		return false
